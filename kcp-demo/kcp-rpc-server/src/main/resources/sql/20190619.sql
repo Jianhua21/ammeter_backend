462c3f8ae090fa757406ec9e58d9c22249b739f5
Legend Peng <penghuazi@126.com>
2019-06-25 09:03:24 +0800

    修复Bug

---
+++ kcp-demo/kcp-rpc-server/src/main/resources/sql/20190619.sql
@@ -0,0 +1,37 @@
+INSERT INTO `ammeter_user` (`realname`, `position_id`, `gender`, `department_id`, `job_number`, `login_name`, `login_passwd`, `mobile_phone`, `email`, `user_type`, `status`,
+ `role_id`, `create_by`, `create_time`, `token`, `channel_id`, `open_id`)
+VALUES ( 'jinan12', NULL, '1', 1, '工程师', 'jinan12', '21218CCA77804D2BA1922C33E0151105', '18115690875', '791629333@qq.com', '1', '1',
+ 1, 'system', '2018-11-26', '4RGqK6roYjnKFxgyqMaQHuCJMLgm2oXH', 2, '');
+INSERT INTO `ammeter_user` (`realname`, `position_id`, `gender`, `department_id`, `job_number`, `login_name`, `login_passwd`, `mobile_phone`, `email`, `user_type`, `status`,
+ `role_id`, `create_by`, `create_time`, `token`, `channel_id`, `open_id`)
+VALUES ( 'jinan13', NULL, '1', 1, '工程师', 'jinan13', '21218CCA77804D2BA1922C33E0151105', '18115690875', '791629333@qq.com', '1', '1',
+ 1, 'system', '2018-11-26', '4RGqK6roYjnKFxgyqMaQHuCJMLgm2oXH', 2, '');
+INSERT INTO `ammeter_user` (`realname`, `position_id`, `gender`, `department_id`, `job_number`, `login_name`, `login_passwd`, `mobile_phone`, `email`, `user_type`, `status`,
+ `role_id`, `create_by`, `create_time`, `token`, `channel_id`, `open_id`)
+VALUES ( 'jinan14', NULL, '1', 1, '工程师', 'jinan14', '21218CCA77804D2BA1922C33E0151105', '18115690875', '791629333@qq.com', '1', '1',
+ 1, 'system', '2018-11-26', '4RGqK6roYjnKFxgyqMaQHuCJMLgm2oXH', 2, '');
+INSERT INTO `ammeter_user` (`realname`, `position_id`, `gender`, `department_id`, `job_number`, `login_name`, `login_passwd`, `mobile_phone`, `email`, `user_type`, `status`,
+ `role_id`, `create_by`, `create_time`, `token`, `channel_id`, `open_id`)
+VALUES ( 'jinan15', NULL, '1', 1, '工程师', 'jinan15', '21218CCA77804D2BA1922C33E0151105', '18115690875', '791629333@qq.com', '1', '1',
+ 1, 'system', '2018-11-26', '4RGqK6roYjnKFxgyqMaQHuCJMLgm2oXH', 2, '');
+INSERT INTO `ammeter_user` (`realname`, `position_id`, `gender`, `department_id`, `job_number`, `login_name`, `login_passwd`, `mobile_phone`, `email`, `user_type`, `status`,
+ `role_id`, `create_by`, `create_time`, `token`, `channel_id`, `open_id`)
+VALUES ( 'jinan16', NULL, '1', 1, '工程师', 'jinan16', '21218CCA77804D2BA1922C33E0151105', '18115690875', '791629333@qq.com', '1', '1',
+ 1, 'system', '2018-11-26', '4RGqK6roYjnKFxgyqMaQHuCJMLgm2oXH', 2, '');
+INSERT INTO `ammeter_user` (`realname`, `position_id`, `gender`, `department_id`, `job_number`, `login_name`, `login_passwd`, `mobile_phone`, `email`, `user_type`, `status`,
+ `role_id`, `create_by`, `create_time`, `token`, `channel_id`, `open_id`)
+VALUES ( 'jinan17', NULL, '1', 1, '工程师', 'jinan17', '21218CCA77804D2BA1922C33E0151105', '18115690875', '791629333@qq.com', '1', '1',
+ 1, 'system', '2018-11-26', '4RGqK6roYjnKFxgyqMaQHuCJMLgm2oXH', 2, '');
+INSERT INTO `ammeter_user` (`realname`, `position_id`, `gender`, `department_id`, `job_number`, `login_name`, `login_passwd`, `mobile_phone`, `email`, `user_type`, `status`,
+ `role_id`, `create_by`, `create_time`, `token`, `channel_id`, `open_id`)
+VALUES ( 'jinan18', NULL, '1', 1, '工程师', 'jinan18', '21218CCA77804D2BA1922C33E0151105', '18115690875', '791629333@qq.com', '1', '1',
+ 1, 'system', '2018-11-26', '4RGqK6roYjnKFxgyqMaQHuCJMLgm2oXH', 2, '');
+INSERT INTO `ammeter_user` (`realname`, `position_id`, `gender`, `department_id`, `job_number`, `login_name`, `login_passwd`, `mobile_phone`, `email`, `user_type`, `status`,
+ `role_id`, `create_by`, `create_time`, `token`, `channel_id`, `open_id`)
+VALUES ( 'jinan19', NULL, '1', 1, '工程师', 'jinan19', '21218CCA77804D2BA1922C33E0151105', '18115690875', '791629333@qq.com', '1', '1',
+ 1, 'system', '2018-11-26', '4RGqK6roYjnKFxgyqMaQHuCJMLgm2oXH', 2, '');
+INSERT INTO `ammeter_user` (`realname`, `position_id`, `gender`, `department_id`, `job_number`, `login_name`, `login_passwd`, `mobile_phone`, `email`, `user_type`, `status`,
+ `role_id`, `create_by`, `create_time`, `token`, `channel_id`, `open_id`)
+VALUES ( 'jinan20', NULL, '1', 1, '工程师', 'jinan20', '21218CCA77804D2BA1922C33E0151105', '18115690875', '791629333@qq.com', '1', '1',
+ 1, 'system', '2018-11-26', '4RGqK6roYjnKFxgyqMaQHuCJMLgm2oXH', 2, '');
+
